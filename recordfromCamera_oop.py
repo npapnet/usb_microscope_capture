7b87149998d52c3835cf42761026a51ba7eeec62
N. Papadakis (d-g5-5590) <npapnet@gmail.com>
2023-07-01 13:16:13 +0300

    Added Grayscale parameter in the experiment.

--- recordfromCamera_oop.py
+++ recordfromCamera_oop.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import time 
 
 class Camera:
+    INIT_DELAY_S = 2
     def __init__(self, id, width, height):
         self.id = id
         self.width = width
@@ -15,7 +16,7 @@ class Camera:
         self.camera = cv2.VideoCapture(self.id, cv2.CAP_DSHOW)
         self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
-        time.sleep(2)
+        time.sleep(self.INIT_DELAY_S)
 
     def capture_image(self):
         ret, frame = self.camera.read()
@@ -25,6 +26,7 @@ class Camera:
         self.camera.release()
 
 class ImageCapturingExperiment:
+    GRAYSCALE=True
     def __init__(self, camera, delay_ms:int, num_images:int, image_folder:pathlib.Path):
         self.camera = camera
         self.delay_ms = delay_ms
@@ -44,22 +46,26 @@ class ImageCapturingExperiment:
         self.image_counter = 0
 
     def _init_test_folder(self):
-        TEST_TIMESTAMP = now_as_str()
-        self.test_folder =self.image_folder / f"test-{TEST_TIMESTAMP}"
+        self.test_start_timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.test_folder =self.image_folder / f"test-{self.test_start_timestamp}"
         self.test_folder.mkdir(parents=True, exist_ok=True)
         print(self.test_folder)
-        self.test_metadata_fname = self.test_folder / f"metadata_{TEST_TIMESTAMP}.txt"
+        self.test_metadata_fname = self.test_folder / f"metadata_{self.test_start_timestamp}.txt"
         self.metadata_fobj = open(self.test_metadata_fname, "w")
         self.metadata_fobj.write('ID \t elapsed\n')
 
         
 
     def capture_image(self, curr_time_s:float):
-
         elapsed_time = curr_time_s - self.start_timestamp
         frame = self.camera.capture_image()
-        filename = self.test_folder / f"img_{self.image_counter:05d}.png"
-        cv2.imwrite(str(filename.absolute()), frame)
+        
+        if self.GRAYSCALE:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        self._write_img_file(frame)
+
+        # write metadata file
         self.metadata_fobj.write(f"{self.image_counter}\t {elapsed_time:010.3f}\n")
 
         self.last_capture_timestamp = curr_time_s
@@ -67,8 +73,12 @@ class ImageCapturingExperiment:
             
         return frame
 
+    def _write_img_file(self, frame) -> None:
+        filename = self.test_folder / f"img_{self.image_counter:05d}.png"
+        cv2.imwrite(str(filename.absolute()), frame)
 
-    def finalise(self):
+
+    def finalise(self) -> None:
         cv2.destroyAllWindows()
         self.camera.release()
         self.metadata_fobj.close()
@@ -85,8 +95,8 @@ NUM_IMAGES = 150
 IMAGE_DATA_DIR = pathlib.Path('captured_images')
 IMAGE_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-def now_as_str()->str:
-    return  datetime.now().strftime("%Y%m%d-%H%M%S")
+# def now_as_str()->str:
+#     return  datetime.now().strftime("%Y%m%d-%H%M%S")
 
 camera = Camera(id=CAMERA_ID, width=CAMERA_WIDTH, height=CAMERA_HEIGHT)
 experiment = ImageCapturingExperiment(camera, DELAY_MS, NUM_IMAGES, image_folder=IMAGE_DATA_DIR)
